3dad1aee16b7f26699508d0b77407ad4f8c67f77
Tom Kristian Tjemsland <tom.tjemsland@bouvet.no>
2019-11-07 12:46:03 +0100

    Rewriting to typescript : wip2

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,14 @@ import {
 
 /**
  * Copy the values of one vector to another without creating a new object.
- * @param {Number[]} source Source vector
- * @param {Number[]} target Target vector
- * @returns {Number[]} Target vector as a copy of source
+ * @param source Source vector
+ * @param target Target vector
+ * @returns Target vector as a copy of source
  *
  * @example
- * // Returns [3, 4]
- * copy([3, 4], [2, 2]);
+ * copy([3, 4], [2, 2]); // Returns [3, 4]
  */
-export function copy(source: number[], target: number[]): number[] {
+export function copy(source: any, target: any): any {
   for (let i = 0; i < source.length; i++) {
     target[i] = source[i];
   }
@@ -26,17 +25,15 @@ export function copy(source: number[], target: number[]): number[] {
  *
  * Component-wise addition of two n-dimensional vectors.
  * Target is used to store the results.
- * @param {Number[]} a Left operand
- * @param {Number[]} b Right operand
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Resulting vector
+ * @param a Left operand
+ * @param b Right operand
+ * @param target Target for storing the results (Default: a)
+ * @return Resulting vector
  *
  * @example
- * // Returns [4, 6]
- * add([1, 2], [3, 4], new Array(2));
+ * add([1, 2], [3, 4], new Array(2)); // Returns [4, 6]
  */
-export function add(a, b, target) {
-  if (!target) target = a;
+export function add(a: any, b: any, target: any = a): any {
   for (let i = 0; i < a.length; i++) {
     target[i] = a[i] + b[i];
   }
@@ -48,17 +45,15 @@ export function add(a, b, target) {
  *
  * Component-wise addition of M n-dimensional vectors. Target is
  * used to store the results.
- * @param {Number[][]} vectors Array of vectors with length n.
- * @param {Number[]} [target=[vectors]] Target for storing the results (Default: vectors[0])
- * @return {Number[]} Resulting vector
+ * @param vectors Array of vectors with length n.
+ * @param target Target for storing the results (Default: vectors[0])
+ * @return Resulting vector
  *
  * @example
- * // Returns [9, 12]
- * addAll([ [1, 2], [3, 4], [5, 6] ], new Array(2));
+ * addAll([ [1, 2], [3, 4], [5, 6] ], new Array(2)); // Returns [9, 12]
  */
-export function addAll(vectors, target) {
-  if (target) copy(vectors[0], target);
-  else [target] = vectors;
+export function addAll(vectors: any[], target: any = vectors[0]): any {
+  copy(vectors[0], target);
   for (let m = 1; m < vectors.length; m++) {
     for (let n = 0; n < target.length; n++) {
       target[n] += vectors[m][n];
@@ -72,17 +67,15 @@ export function addAll(vectors, target) {
  *
  * Component-wise subtraction of two n-dimensional vectors.
  * Target is used to store the results.
- * @param {Number[]} a Left operand
- * @param {Number[]} b Right operand
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Resulting vector
+ * @param a Left operand
+ * @param b Right operand
+ * @param target Target for storing the results (Default: a)
+ * @return Resulting vector
  *
  * @example
- * // Returns [2, 2]
- * sub([4, 3], [2, 1], new Array(2));
+ * sub([4, 3], [2, 1], new Array(2)); // Returns [2, 2]
  */
-export function sub(a, b, target) {
-  if (!target) target = a;
+export function sub(a: any, b: any, target: any = a): any {
   for (let i = 0; i < a.length; i++) {
     target[i] = a[i] - b[i];
   }
@@ -94,18 +87,16 @@ export function sub(a, b, target) {
  *
  * Component-wise subtraction of M n-dimensional vectors from a. Target is
  * used to store the results.
- * @param {Number[]} a Vector with length n.
- * @param {Number[][]} vectors Array of vectors with length n.
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Resulting vector
+ * @param a Vector with length n.
+ * @param vectors Array of vectors with length n.
+ * @param target Target for storing the results (Default: a)
+ * @return Resulting vector
  *
  * @example
- * // Returns [7, 4]
- * subAll([9, 9], [ [2, 3], [0, 2] ], new Array(2));
+ * subAll([9, 9], [ [2, 3], [0, 2] ], new Array(2)); // Returns [7, 4]
  */
-export function subAll(a, vectors, target) {
-  if (target) copy(a, target);
-  else target = a;
+export function subAll(a: any, vectors: any[], target: any = a): any {
+  copy(a, target);
   for (let m = 0; m < vectors.length; m++) {
     for (let n = 0; n < target.length; n++) {
       target[n] -= vectors[m][n];
@@ -116,17 +107,15 @@ export function subAll(a, vectors, target) {
 
 /**
  * Component-wise scaling of vector.
- * @param {Number[]} a Vector to scale
- * @param {Number} factor Scaling factor
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Resulting vector
+ * @param a Vector to scale
+ * @param factor Scaling factor
+ * @param target Target for storing the results (Default: a)
+ * @return Resulting vector
  *
  * @example
- * // Returns [2, 4, 6]
- * scale([1, 2, 3], 2, new Array(3));
+ * scale([1, 2, 3], 2, new Array(3)); // Returns [2, 4, 6]
  */
-export function scale(a, factor, target) {
-  if (!target) target = a;
+export function scale(a: any, factor: number, target: any = a): any {
   for (let i = 0; i < a.length; i++) {
     target[i] = a[i] * factor;
   }
@@ -135,14 +124,13 @@ export function scale(a, factor, target) {
 
 /**
  * Computes the sum of squares
- * @param {Number[]} a Target vector
- * @return {Number} Sum of squares
+ * @param a Target vector
+ * @return Sum of squares
  *
  * @example
- * // Returns 14
- * sumsqr([1, 2, 3]);
+ * sumsqr([1, 2, 3]); // Returns 14
  */
-export function sumsqr(a) {
+export function sumsqr(a: any): number {
   let sum = 0;
   for (let i = 0; i < a.length; i++) {
     sum += a[i] ** 2;
@@ -152,14 +140,13 @@ export function sumsqr(a) {
 
 /**
  * Computes the magnitude of a vector.
- * @param {Number[]} a Target vector
- * @return {Number} Magnitude of vector
+ * @param a Target vector
+ * @return Magnitude of vector
  *
  * @example
- * // Returns 5
- * magnitude([3, 4]);
+ * magnitude([3, 4]); // Returns 5
  */
-export function magnitude(a) {
+export function magnitude(a: any): number {
   const sq = sumsqr(a);
   if (sq === 0) return sq;
   return Math.sqrt(sq);
@@ -167,15 +154,14 @@ export function magnitude(a) {
 
 /**
  * Normalizes a vector
- * @param {Number[]} a Vector to normalize
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Normalized vector
+ * @param a Vector to normalize
+ * @param target Target for storing the results (Default: a)
+ * @return Normalized vector
  *
  * @example
- * // Returns [0, 1, 0]
- * magnitude([0, 10, 0], new Array(3));
+ * magnitude([0, 10, 0], new Array(3)); // Returns [0, 1, 0]
  */
-export function normalize(a, target) {
+export function normalize(a: any, target: any = a): any {
   const len = magnitude(a);
   const f = len === 0 ? 0 : 1 / len;
   return scale(a, f, target);
@@ -183,45 +169,41 @@ export function normalize(a, target) {
 
 /**
  * Get the vector going from b to a.
- * @param {Number[]} a Start coordinates
- * @param {Number[]} b End coordinates
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Vector going from a to b
+ * @param a Start coordinates
+ * @param b End coordinates
+ * @param target Target for storing the results (Default: a)
+ * @return Vector going from a to b
  *
  * @example
- * // Returns [1, -1]
- * dir([2, 1], [3, 0], new Array(2));
+ * dir([2, 1], [3, 0], new Array(2)); // Returns [1, -1]
  */
-export function dir(a, b, target) {
-  if (!target) target = a;
+export function dir(a: any, b: any, target: any = a): any {
   return sub(b, a, target);
 }
 
 /**
  * Calculate the distance between two coordinates.
- * @param {Number[]} a Start coordinates
- * @param {Number[]} b End coordinates
- * @return {Number} Distance between coordinates
+ * @param a Start coordinates
+ * @param b End coordinates
+ * @return Distance between coordinates
  *
  * @example
- * // Returns 5
- * dist([1, 2], [4, 6]);
+ * dist([1, 2], [4, 6]); // Returns 5
  */
-export function dist(a, b) {
+export function dist(a: any, b: any): number {
   return magnitude(sub(b, a));
 }
 
 /**
  * Calculate the dot product between two vectors.
- * @param {Number[]} a Left operand
- * @param {Number[]} b Right operand
- * @return {Number} Dot product
+ * @param a Left operand
+ * @param b Right operand
+ * @return Dot product
  *
  * @example
- * // Returns 11
- * dist([1, 2], [3, 4]);
+ * dist([1, 2], [3, 4]); // Returns 11
  */
-export function dot(a, b) {
+export function dot(a: any, b: any): number {
   let sum = 0;
   for (let i = 0; i < a.length; i++) {
     sum += a[i] * b[i];
@@ -233,17 +215,15 @@ export function dot(a, b) {
  * a × b
  *
  * Find cross product of vectors. Only defined for 3d vectors.
- * @param {Number[]} a Left operand (3d vector)
- * @param {Number[]} b Right operand (3d vector)
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Cross product of vectors
+ * @param a Left operand (3d vector)
+ * @param b Right operand (3d vector)
+ * @param target Target for storing the results (Default: a)
+ * @return Cross product of vectors
  *
  * @example
- * // Returns [0, 0, 1]
- * cross([1, 0, 0], [0, 1, 0]);
+ * cross([1, 0, 0], [0, 1, 0]); // Returns [0, 0, 1]
  */
-export function cross(a, b, target) {
-  if (!target) target = a;
+export function cross(a: any, b: any, target: any = a): any {
   const y = (a[2] * b[0]) - (a[0] * b[2]);
   const z = (a[0] * b[1]) - (a[1] * b[0]);
   target[0] = (a[1] * b[2]) - (a[2] * b[1]);
@@ -256,35 +236,30 @@ export function cross(a, b, target) {
  * a ∙ b × c
  *
  * Find triple product between three vectors. Only defined for 3d vectors.
- * @param {Number[]} a Left operand for dot product (3d vector)
- * @param {Number[]} b Left operand for cross product (3d vector)
- * @param {Number[]} c Right operand for cross product (3d vector)
- * @return {Number} Triple product of vectors
+ * @param a Left operand for dot product (3d vector)
+ * @param b Left operand for cross product (3d vector)
+ * @param c Right operand for cross product (3d vector)
+ * @return Triple product of vectors
  *
  * @example
- * // Returns 1
- * triple([1, 0, 0], [0, 1, 0], [0, 0, 1]);
+ * triple([1, 0, 0], [0, 1, 0], [0, 0, 1]); // Returns 1
  */
-export function triple(a, b, c) {
+export function triple(a: any, b: any, c: any): number {
   return dot(a, cross(b, c));
 }
 
 /**
  * Clamp all values of a vector
- * @param {Number[]} a Values to clamp
- * @param {Number} [min=0] Minimum value (Default: 0)
- * @param {Number} [max=1] Maximum value (Default: 1)
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} Vector with clamped values
+ * @param a Values to clamp
+ * @param min Minimum value (Default: 0)
+ * @param max Maximum value (Default: 1)
+ * @param target Target for storing the results (Default: a)
+ * @return Vector with clamped values
  *
  * @example
- * // Returns [1, 1, 2, 2]
- * clamp([0, 1, 2, 3], 1, 2, new Array(4));
+ * clamp([0, 1, 2, 3], 1, 2, new Array(4)); // Returns [1, 1, 2, 2]
  */
-export function clamp(a, min, max, target) {
-  if (min === undefined) min = 0;
-  if (max === undefined) max = 1;
-  if (!target) target = a;
+export function clamp(a: any, min: number = 0, max: number = 1, target: any = a): any {
   for (let i = 0; i < a.length; i++) {
     target[i] = clampNum(a[i], min, max);
   }
@@ -294,17 +269,15 @@ export function clamp(a, min, max, target) {
 
 /**
  * GLSL step for all values of a vector
- * @param {Number[]} edges Edges of the step function
- * @param {Number} x Value used to generate the step function
- * @param {Number[]} [target=edges] Target for storing the results (Default: edges)
- * @return {Number[]} results for each value in edges
+ * @param edges Edges of the step function
+ * @param x Value used to generate the step function
+ * @param target Target for storing the results (Default: edges)
+ * @return results for each value in edges
  *
  * @example
- * // Returns [1, 1, 0, 0]
- * step([0, 1, 2, 3], 1.5, new Array(4));
+ * step([0, 1, 2, 3], 1.5, new Array(4)); // Returns [1, 1, 0, 0]
  */
-export function step(edges, x, target) {
-  if (!target) target = edges;
+export function step(edges: any, x: number, target: any = edges): any {
   for (let i = 0; i < edges.length; i++) {
     target[i] = stepNum(edges[i], x);
   }
@@ -313,18 +286,16 @@ export function step(edges, x, target) {
 
 /**
  * Mix (interpolate) vectors (similar to glsl implementation).
- * @param {Number[]} a Vector to interpolate from
- * @param {Number[]} b Vector to interpolate to
- * @param {Number} t Interpolation parameter, 0 = a and 1 = b
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number[]} The interpolated vector
+ * @param a Vector to interpolate from
+ * @param b Vector to interpolate to
+ * @param t Interpolation parameter, 0 = a and 1 = b
+ * @param target Target for storing the results (Default: a)
+ * @return The interpolated vector
  *
  * @example
- * // Returns [2, 4]
- * mix([1, 3], [3, 5], 0.5, new Array(2));
+ * mix([1, 3], [3, 5], 0.5, new Array(2)); // Returns [2, 4]
  */
-export function mix(a, b, t, target) {
-  if (!target) target = a;
+export function mix(a: any, b: any, t: number, target: any = a): any {
   const n = clamp(t, 0, 1);
   const m = 1 - n;
   for (let i = 0; i < a.length; i++) {
@@ -335,18 +306,15 @@ export function mix(a, b, t, target) {
 
 /**
  * Rounds all values of a vector to a specific amount of digits.
- * @param {Number[]} a Vector to round
- * @param {Number} [digits=1] Number of digits (Default: 1)
- * @param {Number[]} [target=a] Target for storing the results (Default: a)
- * @return {Number} Vector with rounded values
+ * @param a Vector to round
+ * @param digits Number of digits (Default: 1)
+ * @param target Target for storing the results (Default: a)
+ * @return Vector with rounded values
  *
  * @example
- * // Returns [2.1, 1.2]
- * round([2.12, 1.15], 1, new Array(2));
+ * round([2.12, 1.15], 1, new Array(2)); // Returns [2.1, 1.2]
  */
-export function round(a, digits, target) {
-  if (digits === undefined) digits = 1;
-  if (!target) target = a;
+export function round(a: any, digits: number = 1, target: any = a): number {
   const f = 10 ** digits;
   for (let i = 0; i < a.length; i++) {
     target[i] = Math.round(a[i] * f) / f;
@@ -356,15 +324,14 @@ export function round(a, digits, target) {
 
 /**
  * Returns true if all elements of a vector is zero, otherwise returns false.
- * @param {Number[]} a Target vector
- * @param {Number} [epsilon=0] Accepted deviation from 0.00 (Default: 0)
- * @returns {Boolean} Is target zero vector?
+ * @param a Target vector
+ * @param epsilon Accepted deviation from 0.00 (Default: 0)
+ * @returns Is target zero vector?
  *
  * @example
- * // Returns true
- * isZeroVector([0, 0.000023, 0], 0.001);
+ * isZeroVector([0, 0.000023, 0], 0.001); // Returns true
  */
-export function isZeroVector(a, epsilon) {
+export function isZeroVector(a: any, epsilon: number = 0): boolean {
   if (epsilon === undefined) epsilon = 0;
   for (let i = 0; i < a.length; i++) {
     if (Math.abs(a[i]) > epsilon) return false;
@@ -374,14 +341,13 @@ export function isZeroVector(a, epsilon) {
 
 /**
  * Flatten a collection of vectors to a single array.
- * @param {Number[][]} vectors Array of vectors
- * @return {Number[]} Single array with all values
+ * @param vectors Array of vectors
+ * @return Single array with all values
  *
  * @example
- * // Returns [1, 2, 3, 4, 5, 6]
- * flatten([ [1, 2], [3, 4], [5, 6] ]);
+ * flatten([ [1, 2], [3, 4], [5, 6] ]); // Returns [1, 2, 3, 4, 5, 6]
  */
-export function flatten(vectors) {
+export function flatten(vectors: number[][]): number[] {
   const output = [];
   for (let m = 0; m < vectors.length; m++) {
     for (let n = 0; n < vectors[m].length; n++) {
@@ -393,14 +359,14 @@ export function flatten(vectors) {
 
 /**
  * Reshapes an array of values to a collection of vectors with given dimensions.
- * @param {Number[]} array Single array with all values
- * @return {Number[][]} Array of vectors with given dimensions
+ * @param array Single array with all values
+ * @param dimensions Dimensions per vector
+ * @return Array of vectors with given dimensions
  *
  * @example
- * // Returns [ [1, 2, 3], [4, 5, 6] ]
- * reshape([1, 2, 3, 4, 5, 6], 3);
+ * reshape([1, 2, 3, 4, 5, 6], 3); // Returns [ [1, 2, 3], [4, 5, 6] ]
  */
-export function reshape(array, dimensions) {
+export function reshape(array: number[], dimensions: number): number[][] {
   const output = [];
   for (let i = 0; i < array.length; i += dimensions) {
     const vector = new Array(dimensions);
